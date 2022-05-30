1c6d6ed20fe537dc0fb44800a6288395aaac8976
radins <felipe.radins@korp.com.br>
2022-05-30 11:47:07 -0300

    wip

--- setup.sh
+++ setup.sh
@@ -1,52 +1,109 @@
-#clone repo
+#!/bin/bash
+
+
+create_random_string() {
+  local l=15
+  [ -n "$1" ] && l=$1
+  [ -n "$2" ] && l=$(shuf --random-source=/dev/urandom -i $1-$2 -n 1)
+  tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs
+}
+
 
 # Válida se o script está sendo rodado como root
 if [ $(/usr/bin/id -u) -ne 0 ]; then
-    echo "Por favor, execute o scrip como administrador"
-    exit 01
+    echo "$(tput setaf 3)Por favor, execute o scrip como administrador.$(tput setaf 7)"
 fi
+
+
+is_first_install=False
+
+
+# Confirma que o script tem o tenant  - NÃO valida a GUID
+if ! test -f /etc/korp/tenant  && [ $# = 0 ] ;
+then
+    echo "$(tput setaf 1)Setup sendo executado pela primeira vez, porém o tenant não foi passado.$(tput setaf 7)"
+else
+    if test -f /etc/korp/tenant;
+    then
+        if [ $# = 1 ];
+        then
+            if [ "$(cat /etc/korp/tenant)" != "$1" ];
+            then
+                echo ""
+                echo "$(tput setaf 1)O setup já foi rodado uma vez com outro tenant. Por favor vefique o tenant e tente novamente.$(tput setaf 7)"
+            fi
+        fi
+    else
+        is_first_install=True
+        sudo sudo mkdir -p /etc/korp/
+        sudo echo $1 > /etc/korp/tenant
+        sudo chmod 0444 /etc/korp/tenant        
+    fi
+fi
+tenant=$(cat /etc/korp/tenant)
+
 
 # Atualização de repositório, instalação de dependencias, isntalação de ansible
 echo Instalando Ansible
 
-# sudo apt update
-# sudo apt install software-properties-common
-# # Testar add-apt-repository
-# sudo add-apt-repository --yes --update ppa:ansible/ansible
-# sudo apt install ansible -y
+sudo apt update
+sudo apt install software-properties-common
+sudo apt install ansible -y
 
-echo "\n-----------------------\n"
-echo "Para continuar a instalação, digite as seguintes informações sobre o servidor SQL Server:"
-echo "IP de acesso: "
-read sql_ip
-echo "Usuário com permissões administrativas: "
-read sql_user
-echo "Senha do usuário: "
-read sql_pass
-echo "\nAgora, digite as seguintes informações sobre o servidor Linux"
-echo "Usuário com permissões administrativas: "
-read linux_user
-echo "Senha do usuário: "
-read linux_pass
-# TODO IP do servicor
 
-# cria arquivo 'ansible-vars.json' com base nas respostas das perguntas anteriores
-printf """
+# Caso seja a primeira instalação, irá os arquivos/configurações nocessários(as)
+if [ $is_first_install = True ];
+then
+
+    # Cria e configuração de diretórios que seram usados depois
+    sudo mkdir -p /etc/ansible/hosts/
+    sudo chmod 0774 /etc/ansible/
+
+    echo "\n-----------------------\n"
+    echo "Para continuar a instalação, digite as seguintes informações sobre o servidor SQL Server:"
+    echo "IP de acesso: "
+    read sql_ip
+    echo "Usuário com permissões administrativas: "
+    read sql_user
+    echo "Senha do usuário: "
+    read sql_pass
+
+
+    # Criação de senhas aleatórios para o usuário do mssql e do linux
+    mssql_korp_pass="$(create_random_string)"
+    linux_korp_pass="$(create_random_string)"
+
+
+    # Cria arquivo 'ansible-vars.json' com base nas respostas das perguntas anteriores
+    printf """
 all:
   children:
     nodes:
       hosts:
-        192.168.1.107:
+        localhost:
           mssql:
             address: $sql_ip
-            user: $sql_user
-            password: $sql_pass
-          linux:
-            user: $linux_user
-            password: $linux_pass
-"""> ansible-inventory.yml
+            default_user: $sql_user
+            default_password: $sql_pass
+            korp_user: korp.services
+            korp_password: $mssql_korp_pass
+          linux_korp:
+            user: korp
+            password: $linux_korp_pass
 
-# sudo ansible-playbook provisioning_playbook.yml --inventory-file ansible-inventory.yml
-sudo ansible-pull -U https://github.com/viasoftkorp/KorpSetupLinux.git -C TD-828 --inventory-file ansible-inventory.yml provisioning_playbook.yml
+""" > /etc/ansible/hosts/ansible-inventory.yml
 
 
+    # Criação de senha aleatória usada pelo ansible-vault
+    sudo echo $(create_random_string) > /etc/ansible/.vault_key
+    sudo chown root:root /etc/ansible/.vault_key
+    sudo chmod 400 /etc/ansible/.vault_key
+
+
+    # Encripta 'ansible-inventory.yml' com ansible-vault
+    sudo ansible-vault encrypt /etc/ansible/hosts/ansible-inventory.yml --vault-id /etc/ansible/.vault_key
+fi
+
+
+# '--limit localhost' é necessário pois 'ansible-pull' dará um erro de host não especificato com isso
+sudo ansible-pull -U https://github.com/viasoftkorp/KorpSetupLinux.git -C TD-828 main.yml --limit localhost --vault-id /etc/ansible/.vault_key
