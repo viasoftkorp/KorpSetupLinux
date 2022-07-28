1c6fcc423ed2b1e76482a4028012c2f0218fe081
radins <felipe.radins@korp.com.br>
2022-07-28 10:50:35 -0300

    alteração no nome de variáveis

--- roles/utils/library/consul_kv.py
+++ roles/utils/library/consul_kv.py
@@ -6,19 +6,19 @@ from ansible.module_utils.basic import *
 def main():
     module = AnsibleModule(
         argument_spec= dict(
-            current_consul_json = dict(type='dict', required=True),
-            new_consul_json = dict(type='dict', required=True)
+            current_kv = dict(type='dict', required=True),
+            new_kv = dict(type='dict', required=True)
         )
     )
 
-    new_consul_json = module.params['new_consul_json']
-    current_consul_json = module.params['current_consul_json']
+    new_kv = module.params['new_kv']
+    current_kv = module.params['current_kv']
 
-    new_consul_json.update(current_consul_json)
+    new_kv.update(current_kv)
 
     result = dict(
         changed=False,
-        prop=new_consul_json,
+        prop=new_kv,
     )
 
     module.exit_json(**result)
