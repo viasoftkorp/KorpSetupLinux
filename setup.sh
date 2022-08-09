11ce7df267d4414426e2d64d867fd428a8a3afe6
radins <felipe.radins@korp.com.br>
2022-08-09 16:08:20 -0300

    TD-914
    adição de script de reload de containers

--- setup.sh
+++ setup.sh
@@ -85,6 +85,11 @@ echo Instalando Ansible
 
 # caso o comando falhe, checar 'https://askubuntu.com/questions/1123177/sudo-add-apt-repository-hangs'
 sudo add-apt-repository --yes --update ppa:ansible/ansible
+if [ $? != 0 ]
+then
+    echo "$(tput setaf 1)Erro 'sudo add-apt-repository --yes --update ppa:ansible/ansibl'.$(tput setaf 7)"
+    exit 12
+fi
 sudo apt install ansible --yes
 
 
