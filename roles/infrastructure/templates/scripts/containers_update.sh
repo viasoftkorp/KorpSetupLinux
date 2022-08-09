88003e4f18736e28297a6bdb2c7917ba813877eb
Felipe Radins <61010938+Radins@users.noreply.github.com>
2022-08-09 16:18:28 -0300

    TD-914 (#94)
    TD-914

---
+++ roles/infrastructure/templates/scripts/containers_update.sh
@@ -0,0 +1,10 @@
+echo "$(tput setaf 2)Atualizando containers...$(tput setaf 7)"
+
+sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --run-once
+if [ $? != 0 ]
+then
+    echo "$(tput setaf 1)Erro durante atualização de containers.$(tput setaf 7)"
+    exit 01
+fi
+
+echo "$(tput setaf 2)Containers atualizados$(tput setaf 7)"
