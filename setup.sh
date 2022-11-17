0e4fe0625c136cda86d5331c44739f72e2f3fcd0
gustavoaragao <gustavo.aragao@korp.com.br>
2022-11-17 13:37:20 -0300

    remoção suffix_divider de customs

--- setup.sh
+++ setup.sh
@@ -18,10 +18,9 @@ create_random_string() {
 #   run_bootstrap=false - ira rodar o main.yml e não bootstrap-playbook.yml   (padrão true)
 #   custom_tags="<tag1,tag2>" - OPCIONAL, caso não sejá passada, as tags "default-setup,install" serão usadas
 #   db_suffix="<db_suffix>" - OPCINOAL
-#   db_suffix_divider="<db_suffix_divider>" - OPCINOAL
 
 
-install_apps=""; docker_account=""; ansible_tags=""; dns_api=""; dns_frontend=""; dns_cdn=""; db_suffix="";db_suffix_divider=""
+install_apps=""; docker_account=""; ansible_tags=""; dns_api=""; dns_frontend=""; dns_cdn=""; db_suffix="";
 run_bootstrap="True"
 ini_file_path="./setup_config.ini"
 
@@ -30,7 +29,6 @@ then
     install_apps=$(sed -nr "/^\[OPTIONS\]/ { :l /^install_apps[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
     docker_account=$(sed -nr "/^\[OPTIONS\]/ { :l /^docker_account[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)  
     db_suffix=$(sed -nr "/^\[OPTIONS\]/ { :l /^db_suffix[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
-    db_suffix_divider=$(sed -nr "/^\[OPTIONS\]/ { :l /^db_suffix_divider[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
     dns_api=$(sed -nr "/^\[OPTIONS\]/ { :l /^dns_api[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
     dns_frontend=$(sed -nr "/^\[OPTIONS\]/ { :l /^dns_frontend[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
     dns_cdn=$(sed -nr "/^\[OPTIONS\]/ { :l /^dns_cdn[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" $ini_file_path)
@@ -241,7 +239,6 @@ ansible-pull -U https://github.com/viasoftkorp/KorpSetupLinux.git $playbook_name
     "customs": {
       "docker_account": "'$docker_account'",
       "db_suffix": "'$db_suffix'",
-      "db_suffix_divider": "'$db_suffix_divider'",
       "frontend": {
         "dns": {
           "api": "'$dns_api'",
