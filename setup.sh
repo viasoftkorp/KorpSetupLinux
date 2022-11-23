51aaf694bf7ca2268b85ce6b14955b911737ece1
Victor Nico <72225869+victornico15@users.noreply.github.com>
2022-11-23 13:55:16 -0300

    Devops 80 (#163)
    alterado logica de inventário

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ create_random_string() {
 #   install_apps="<apps1,apps2>"
 #   run_bootstrap=false - ira rodar o main.yml e não bootstrap-playbook.yml   (padrão true)
 #   custom_tags="<tag1,tag2>" - OPCIONAL, caso não sejá passada, as tags "default-setup,install" serão usadas
+#   # variaveis salvas no inventário:
 #   db_suffix="<db_suffix>" - OPCIONAL, sufixo utilizado na criação dos bancos e nas ConnectionStrings do Consul KV
 
 
@@ -46,7 +47,7 @@ else
       exit 0
     fi
 fi
-    
+
 for ARGUMENT in "$@"
 do
    KEY=$(echo $ARGUMENT | cut -f1 -d=)
@@ -81,13 +82,14 @@ then
     exit 08
 else
     status_code=$(curl -X GET -o /dev/null --silent "$gateway_url/TenantManagement/server-deploy/token/$token" --write-out '%{http_code}\n')
-    
+
     if [ "$status_code" != "200" ];
     then
         echo "$(tput setaf 1)O token passado não é válido. Status Code: $status_code.$(tput setaf 7)"
         exit 09
     fi
 fi
+
 
 # Atualização de repositório, instalação de dependencias, isntalação de ansible
 
@@ -126,111 +128,52 @@ else
     fi
 fi
 
-
-# Validação do arquivo de inventário para saber se o setup está sendo rodado pela primeira vez, ou não
-
-is_first_install=False
-
-if ! sudo test -f /etc/korp/ansible/inventory.yml ;
+# Entrará nesse 'if' caso o arquivo de inventário não exista
+if ! sudo test -f /etc/korp/ansible/inventory.yml;
 then
-    is_first_install=True
-fi
-
-
-# Caso seja a primeira instalação, irá gerar os arquivos/configurações nocessários(as)
-if [ $is_first_install = True ];
-then
-
     # Cria e diretórios que serão usados depois
     sudo mkdir -p /etc/korp/ansible/
-
-    echo -e "\n-----------------------\n"
-    echo "Para continuar a instalação, digite as seguintes informações sobre o servidor SQL Server:"
-    read -e -p "IP de acesso: " sql_ip
-    read -e -p "Usuário com permissões administrativas: " sql_user
-    read -e -p "Senha do usuário: " sql_pass
-    read -p "Agora, informe o IP do Servidor de aplicações (ou pressione enter para usar '$sql_ip'): " application_server_address
-    application_server_address=${application_server_address:-$sql_ip}
-
-
-    # Criação de senhas aleatórios para o usuário do mssql, postgres e do linux
-    mssql_korp_pass="$(create_random_string)"
-    postgres_korp_pass="$(create_random_string)"
-    linux_korp_pass="$(create_random_string)"
-    rabbitmq_korp_pass="$(create_random_string)"
-    redis_pass="$(create_random_string)"
-    minio_access_key="$(create_random_string)"
-    minio_secret_key="$(create_random_string)"
-
-    db_suffix_divider="_"; mssql_korp_user_suffix=""
-    if [ "$db_suffix" != "" ];
-    then
-      mssql_korp_user_suffix="$db_suffix_divider$db_suffix"
-    fi
-
-    # Cria arquivo 'ansible-vars.json' com base nas respostas das perguntas anteriores, e nas senhas geradas
-    echo """
-all:
-  children:
-    nodes:
-      hosts:
-        localhost:
-          app_server:
-            address: $application_server_address
-          linux_korp:
-            user: korp
-            password: $linux_korp_pass
-          self_signed_cert:
-            passphrase: korp
-          mssql:
-            address: $sql_ip
-            default_user: $sql_user
-            default_password: $sql_pass
-            korp_user: korp.services$mssql_korp_user_suffix
-            korp_password: $mssql_korp_pass
-          postgres:
-            address: 127.0.0.1
-            default_user: postgres
-            default_password: postgres
-            korp_user: korp.services
-            korp_password: $postgres_korp_pass
-          rabbitmq:
-            korp_user: korp.services
-            korp_password: $rabbitmq_korp_pass
-          redis:
-            password: $redis_pass
-          minio:
-            access_key: $minio_access_key
-            secret_key: $minio_secret_key
-          general:
-            introspection_secret: $(cat /proc/sys/kernel/random/uuid)
-          docker_servicos_network_ip_address_start: 172.18
-          db_suffix_divider: $db_suffix_divider
-          db_suffix: "$db_suffix"
-
-""" | sudo tee /etc/korp/ansible/inventory.yml > /dev/null
-
-    sudo chmod 644 /etc/korp/ansible/inventory.yml
-
-    echo """
-[defaults]
-inventory = /etc/korp/ansible/inventory.yml
-""" | sudo tee /etc/ansible/ansible.cfg > /dev/null
-
     # Criação de senha aleatória usada pelo ansible-vault
     echo $(create_random_string) | sudo tee /etc/korp/ansible/.vault_key > /dev/null
-    sudo chown root:root /etc/korp/ansible/.vault_key
-
-    # Encripta 'inventory.yml' com ansible-vault
-    sudo ansible-vault encrypt /etc/korp/ansible/inventory.yml --vault-id /etc/korp/ansible/.vault_key
-
+    sudo chown root:root /etc/korp/ansible/.vault_key  
+    # Altera a permissão de .vault_key
+    sudo chmod 444 /etc/korp/ansible/.vault_key
+    # Cria inventory.yml
+    sudo touch /etc/korp/ansible/inventory.yml 
     # Corrige a permição dos arquivos
     sudo chmod 644 /etc/korp/ansible/inventory.yml
-    sudo chmod 444 /etc/korp/ansible/.vault_key
-    
+    # Configuração de '/etc/ansible/ansible.cfg' para apontar o inventário para '/etc/korp/ansible/inventory.yml'
+    echo """  
+    [defaults]
+    inventory = /etc/korp/ansible/inventory.yml
+    """ | sudo tee /etc/ansible/ansible.cfg > /dev/null
+    # Encripta 'inventory.yml' com ansible-vault
+    sudo ansible-vault encrypt /etc/korp/ansible/inventory.yml --vault-id /etc/korp/ansible/.vault_key
+    sudo chmod 644 /etc/korp/ansible/inventory.yml
 fi
 
-# verificação caso não deseja que rode o bootstrap-playbook.yml
+# Download de inventory-playbook.yml pois 'ansible-pull' não suporta o módulo 'ansible.builtin.pause'
+wget -P /tmp https://raw.githubusercontent.com/viasoftkorp/KorpSetupLinux/$branch_name/inventory-playbook.yml
+
+ansible-playbook /tmp/inventory-playbook.yml --vault-id /etc/korp/ansible/.vault_key \
+  --extra-vars='{
+    "db_suffix": "'$db_suffix'"
+  }'
+
+if [ $? != 0 ]
+then
+    echo "$(tput setaf 1)Erro durante a execução do playbook 'inventory-playbook.yml'.$(tput setaf 7)"
+    exit 14
+fi
+
+# Encripta 'inventory.yml' com ansible-vault visto que essa operação não pode ser feita no playbook
+sudo ansible-vault encrypt /etc/korp/ansible/inventory.yml --vault-id /etc/korp/ansible/.vault_key
+sudo chmod 644 /etc/korp/ansible/inventory.yml
+
+rm /tmp/inventory-playbook.yml
+
+
+# Verificação caso não deseja que rode o bootstrap-playbook.yml
 playbook_name=""
 if [ ${run_bootstrap^^} == "FALSE" ];
 then
@@ -259,7 +202,7 @@ ansible-pull -U https://github.com/viasoftkorp/KorpSetupLinux.git $playbook_name
       }
     },
     "apps":['$install_apps']
-  }' 
+  }'
 
 if [ $? != 0 ]
 then
