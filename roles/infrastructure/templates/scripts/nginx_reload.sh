409cf906014c51ab1797bacee5a3d8f26b920c02
radins <felipe.radins@korp.com.br>
2023-04-26 09:17:18 -0300

    wip

--- roles/infrastructure/templates/scripts/nginx_reload.sh
+++ roles/infrastructure/templates/scripts/nginx_reload.sh
@@ -1,2 +1,2 @@
-docker exec -it nginx bash -c "/usr/sbin/nginx -s reload"
-# TODO log de output
+echo "Iniciando task de reload de nginx - $(date)" >>  {{ certs_directory }}/letsencrypt/logs/nginx_reload.log
+sudo docker exec -it nginx bash -c "/usr/sbin/nginx -s reload" >> {{ certs_directory }}/letsencrypt/logs/nginx_reload.log 2>&1
