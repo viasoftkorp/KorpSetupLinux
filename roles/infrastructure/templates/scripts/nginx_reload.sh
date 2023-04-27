58d83b0a72aaeb9244c04c324aa5eec655c66a48
radins <felipe.radins@korp.com.br>
2023-04-27 13:59:56 -0300

    wip

--- roles/infrastructure/templates/scripts/nginx_reload.sh
+++ roles/infrastructure/templates/scripts/nginx_reload.sh
@@ -1,2 +1,2 @@
 echo "Iniciando task de reload de nginx - $(date)" >>  {{ certs_directory }}/letsencrypt/logs/nginx_reload.log
-docker exec -it nginx bash -c "/usr/sbin/nginx -s reload" >> {{ certs_directory }}/letsencrypt/logs/nginx_reload.log 2>&1
+docker exec nginx bash -c "/usr/sbin/nginx -s reload" >> {{ certs_directory }}/letsencrypt/logs/nginx_reload.log 2>&1
