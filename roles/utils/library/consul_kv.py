041dd35da2e9c8e3bbe2c9889a2d5a84d2cf138c
pedrokorp <140519536+pedrokorp@users.noreply.github.com>
2023-08-25 09:12:43 -0300

    ANL-334-Pedro (#229)
    * ANL-334-Pedro
    Conforme feito com o Lucas Barreto, criando a role para o cliente Viasoft
    
    * correção compose
    
    * compose fix
    
    * Logica para correção de KV de viasoft.elt
    
    * ANL-334-Pedro
    Corrigindo o PR
    
    * ANL-334
    Adicionando tag da notificação
    
    ---------
    
    Co-authored-by: radins <felipe.radins@korp.com.br>

--- roles/utils/library/consul_kv.py
+++ roles/utils/library/consul_kv.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+# Modulo garante que nenhum propriedade já existente no consul seja sobrescrita
+# Caso isso seja alterado irá quebrar a adição de cliente oauth (secret será sobrescrito no consul), e lógica do KV de Viasoft.ELT
+
 from pkg_resources import require
 from ansible.module_utils.basic import *
 
