6b5c9178feb7fca20e18fae8e8b6b923f9281b57
Víctor Pegoraro <victorbonettixd@gmail.com>
2024-02-06 11:49:07 -0300

    melhorando módulo python

--- roles/utils/library/consul_kv.py
+++ roles/utils/library/consul_kv.py
@@ -6,20 +6,9 @@ from ansible.module_utils.basic import *
 # Este módulo permite a manipulação de KVs do consul.
 # Ele realiza as seguintes operações:
 #
-#   1. Adiciona valores inéditos;
-#       
+#   1. Adiciona valores inéditos;      
 #   2. Sobrescreve valores referentes ao caminho passado explicitamente em 'keys_to_overwrite'; 
-#     O caminho é *** relativo ao kv atual ***. Exemplos:
-#       current_kv = a: {b: "Valor antigo1", d: "Valor antigo2"}}
-#       new_kv     = a: {b: {c: "Valor novo"}}
-        # KEYS_TO_OVERWRITE     |    OUTPUT    
-#             ["a.b"]           |  a: {b: {c: "Valor novo"}, d: "Valor antigo2"}}
-#             ["a"]             |  a: {b: {c: "Valor novo"}} 
-#             ["a.b.c"]         |  a: {b: "Valor antigo1", d: "Valor antigo2"}} (sem efeito)
-#
 #   3. Retorna um novo dicionário resultante da operação.
-#
-# Função recursiva para substituir valor da chave a ser atualizada;
 
 def replace_key(keys_sequence, dictionary, new_val):
     current_key = keys_sequence[0]
@@ -53,20 +42,20 @@ def main():
     current_kv = module.params['current_kv']
     keys_to_overwrite = module.params["keys_to_overwrite"]
 
-    temp_kv = new_kv.copy()
-    temp_kv.update(current_kv) # armazena novas chaves
+    merged_kv = new_kv.copy()
+    merged_kv.update(current_kv) # armazena novas chaves
 
     # Removendo chaves antigas se houverem chaves atualizadas
     for key_path in keys_to_overwrite:
         keys_sequence = key_path.split(".")
-        new_val = access_value(keys_sequence, new_kv) # garantir existência de valor no novo dict
+        new_val = access_value(keys_sequence, new_kv)
         if new_val:
-            temp_kv = replace_key(keys_sequence, temp_kv, new_val) 
+            merged_kv = replace_key(keys_sequence, merged_kv, new_val) 
     
     # Retornando resultado
     result = dict(
         changed=False,
-        prop=temp_kv,
+        prop=merged_kv,
     )
    
     module.exit_json(**result)
