73277c689d6eee95500cbb968132c848e8838cd0
Víctor Pegoraro <victorbonettixd@gmail.com>
2024-07-09 10:55:24 -0300

    wip

--- setup.sh
+++ setup.sh
@@ -252,10 +252,10 @@ ansible-playbook /tmp/KorpSetupLinux/bootstrap-playbook.yml \
     "skip_salt_test": '$skip_salt_test'
   }'
 
-sudo rm -r /tmp/KorpSetupLinux
-
 if [ $? != 0 ]
 then
     echo "$(tput setaf 1)Erro durante a execução do playbook 'main.yml'.$(tput setaf 7)"
     exit 11
 fi
+
+sudo rm -r /tmp/KorpSetupLinux
