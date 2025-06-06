0f552e481e437c1e2d29e273dad80e3653bb8f00
radins <felipe.radins@korp.com.br>
2025-06-06 18:57:02 -0300

    add apt install git

--- setup.sh
+++ setup.sh
@@ -132,7 +132,7 @@ then
     echo "$(tput setaf 1)Erro 'sudo add-apt-repository --yes --update ppa:ansible/ansible'.$(tput setaf 7)"
     exit 12
 fi
-sudo apt install ansible --yes
+sudo apt install git ansible --yes
 if [ $? != 0 ]
 then
     echo "$(tput setaf 1)Erro 'sudo apt install ansible --yes'.$(tput setaf 7)"
